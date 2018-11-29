8303d996f3cee4d83eae887e75047d8dbcd4ed76
Benoit Dubreuil <lol12lol12lol12lol12@hotmail.com>
2018-11-29 10:43:44 -0500

    Integrated the MemoryUtils into the StackArrayImpl

--- Exception-Safety_Issues_and_Techniques/Stack/StackArrayImpl.h
+++ Exception-Safety_Issues_and_Techniques/Stack/StackArrayImpl.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include "../MemoryUtils.h"
+
+// For learning purposes, the global operators new and delete are used. Ideally, the std::allocator class should be used.
+template <typename T>
 class StackArrayImpl
 {
+	size_t m_size;
+	size_t m_usedOffset;
+	T* m_data;
 
+public:
+	explicit StackArrayImpl(size_t size = 0) : m_size(size), m_usedOffset(), m_data(size ? operator new(sizeof(T) * size) : nullptr)
+	{
+	}
+
+	~StackArrayImpl()
+	{
+		MemoryUtils::placementDestruct(m_data, m_data + m_usedOffset);
+		operator delete(m_data);
+	}
 };
