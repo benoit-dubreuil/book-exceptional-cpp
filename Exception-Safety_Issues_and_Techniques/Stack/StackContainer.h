cf23fdeff1ecf74a6ddf2c2796b572f340479312
Benoit Dubreuil <lol12lol12lol12lol12@hotmail.com>
2018-11-29 11:51:54 -0500

    Added getters, a swap method, overloaded the operator= and added the move constructor

--- Exception-Safety_Issues_and_Techniques/Stack/StackContainer.h
+++ Exception-Safety_Issues_and_Techniques/Stack/StackContainer.h
@@ -23,4 +23,33 @@ public :
 			++m_impl.usedOffset;
 		}
 	}
+
+	StackContainer(StackContainer&& other) : StackContainer()
+	{
+		swap(*this, other);
+	}
+
+	StackContainer& operator=(StackContainer other)
+	{
+		swap(*this, other);
+
+		return *this;
+	}
+
+	friend void swap(StackContainer& lhs, StackContainer& rhs) noexcept
+	{
+		using std::swap;
+
+		swap(lhs.m_impl, rhs.m_impl);
+	}
+
+	size_t getReservedSize() const
+	{
+		return m_impl.size;
+	}
+
+	size_t getUsedSize() const
+	{
+		return m_impl.usedOffset;
+	}
 };
