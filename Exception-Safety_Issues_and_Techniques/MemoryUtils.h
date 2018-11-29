287139ad2335ecc076460d351f12c6090f0ca37d
Benoit Dubreuil <lol12lol12lol12lol12@hotmail.com>
2018-11-29 10:39:28 -0500

    Added the MemoryUtils static class

---
+++ Exception-Safety_Issues_and_Techniques/MemoryUtils.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class MemoryUtils final
+{
+	MemoryUtils() = delete;
+
+public:
+	
+	template <typename T, typename... TArgs>
+	static void placementNew(T* const ptr, const TArgs&... args)
+	{
+		new (ptr) T(args...);
+	}
+
+	template <typename T>
+	static void placementDestruct(T* const ptr)
+	{
+		ptr->~T();
+	}
+
+	template <class ForwardIterator>
+	void placementDestruct(ForwardIterator first, ForwardIterator last)
+	{
+		while (first != last)
+		{
+			placementDestruct(&*first);
+			++first;
+		}
+	}
+};
