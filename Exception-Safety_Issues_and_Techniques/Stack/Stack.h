4dcd687ae0ae9cd6b15b68430925277e937acfc0
Benoit Dubreuil <lol12lol12lol12lol12@hotmail.com>
2018-12-01 10:13:05 -0500

    Removed the StackLinkedImpl class

--- Exception-Safety_Issues_and_Techniques/Stack/Stack.h
+++ Exception-Safety_Issues_and_Techniques/Stack/Stack.h
@@ -4,13 +4,9 @@
 
 #include "StackContainer.h"
 #include "StackArrayImpl.h"
-#include "StackLinkedImpl.h"
 
 template <typename T>
 using Stack = StackContainer<T, StackArrayImpl<T>>;
 
 template <typename T>
 using StackArray = StackContainer<T, StackArrayImpl<T>>;
-
-template <typename T>
-using StackLinked = StackContainer<T, StackLinkedImpl<T>>;
